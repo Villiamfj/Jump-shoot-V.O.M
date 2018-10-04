997a49e6a98ab70354cb21405afffdc7e0853838
Villiamfj <villiamfj@hotmail.dk>
2018-10-04 14:19:42 +0200

    movement og bullet movement

---
+++ Jump shoot 2d/Jump shoot 2d/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+    private Vector3 mousePosition;
+    public Rigidbody2D rb;
+    public GameObject player;
+    public float Bulletspeed;
+    public float lifetime;
+    public GameObject wall;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 vej = mousePosition - transform.position;
+        rb.velocity = vej*Bulletspeed;
+        Destroy(gameObject, lifetime);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject == wall)
+        {
+            
+        }
+    }
+}
--- Jump shoot 2d/Jump shoot 2d/Assets/Scripts/Movement.cs
+++ Jump shoot 2d/Jump shoot 2d/Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour {
     private Vector3 mousePosition;
     public float moveSpeed;
     public Rigidbody2D rb2D;
+    public GameObject ShotBullet;
 
     bool Midjump;
     // Use this for initialization
@@ -18,7 +19,7 @@ public class Movement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
+        //Jump
         if (Input.GetMouseButtonDown(1) & Midjump == false)
         {
             Midjump = true;
@@ -27,11 +28,18 @@ public class Movement : MonoBehaviour {
             rb2D.AddForce((mousePosition-transform.position)*moveSpeed);
         }
 
+        //shoot
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(ShotBullet, transform.position,Quaternion.identity);
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Midjump = false;
-        //rb2D.velocity = Vector3.zero;
+        rb2D.velocity = Vector3.zero;
+        
         
     }
 }
